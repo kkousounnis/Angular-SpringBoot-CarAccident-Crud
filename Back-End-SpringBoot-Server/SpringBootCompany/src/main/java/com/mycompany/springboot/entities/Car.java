741d321317afb1736881e91cc9a0e1b79cd461d7
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-10-24 12:55:02 -0800

    Updates on Back End Server.

--- Back-End-SpringBoot-Server/SpringBootCompany/src/main/java/com/mycompany/springboot/entities/Car.java
+++ Back-End-SpringBoot-Server/SpringBootCompany/src/main/java/com/mycompany/springboot/entities/Car.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_emp")
+@Table(name = "car_accidents")
 public class Car implements Serializable {
     
     @Id
