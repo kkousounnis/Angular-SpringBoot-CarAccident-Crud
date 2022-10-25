967ef2210dab9cda449909bde18672cd8795a991
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-10-25 03:29:13 -0800

    Updates.

--- Back-End-SpringBoot-Server/SpringBootCompany/src/main/java/com/mycompany/springboot/controllers/CarController.java
+++ Back-End-SpringBoot-Server/SpringBootCompany/src/main/java/com/mycompany/springboot/controllers/CarController.java
@@ -4,10 +4,13 @@ import com.mycompany.springboot.entities.Car;
 import com.mycompany.springboot.entities.dtos.responses.CarResponse;
 import com.mycompany.springboot.services.CarServiceImpl;
 import com.mycompany.springboot.entities.dtos.requests.CarRequest;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -33,37 +36,39 @@ public class CarController {
         return (new CarResponse(carServiceImpl.get()).getCars());
     }
 
-    
+
     @PostMapping("/car")
-    public Car save(@RequestBody CarRequest car) {  //to change to CarRequest issue with angular    
-        
+    public ResponseEntity<Car> save(@RequestBody CarRequest car) {
+
         carServiceImpl.save(car.getCar());
-        return (car.getCar());
+
+        return new ResponseEntity("Car accident created with success.", HttpStatus.CREATED);
     }
-    
-    
+
+
     @GetMapping("/car/{id}")
     public ResponseEntity<Car> get(@PathVariable int id) {
         Car car = carServiceImpl.get(id);
-        return ResponseEntity.ok(car);
+        return new ResponseEntity("Success", HttpStatus.OK);
     }
 
-    
+
     @DeleteMapping("/car/{id}")
     public ResponseEntity<Map<String, Boolean>> delete(@PathVariable int id) {
         carServiceImpl.delete(id);
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", Boolean.TRUE);
-        return ResponseEntity.ok(response);
+        return new ResponseEntity("Deleted with success.", HttpStatus.OK);
     }
 
-    
-    @PutMapping("/employee/{id}")
-    public ResponseEntity<Car> update(@PathVariable int id, @RequestBody Car employeeDetails) {
-        Car car = carServiceImpl.get(id);
 
-        carServiceImpl.save(car);
-        return ResponseEntity.ok(car);
+    @PutMapping("/car/{id}")
+    public ResponseEntity<Car> update(@PathVariable int id, @RequestBody CarRequest employeeDetails) {
+        Car car = carServiceImpl.get(id);
+        employeeDetails.getCar().setId(car.getId());
+        System.err.println("Errors" + employeeDetails.getCar().getBrand());
+        carServiceImpl.save(employeeDetails.getCar());
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
 }
