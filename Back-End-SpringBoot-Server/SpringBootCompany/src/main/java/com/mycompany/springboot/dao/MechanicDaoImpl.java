e19c6597928efba593a05df6b31d23b5df1d8777
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-10-28 11:17:24 -0800

    Update MechanicDaoImpl.java

--- Back-End-SpringBoot-Server/SpringBootCompany/src/main/java/com/mycompany/springboot/dao/MechanicDaoImpl.java
+++ Back-End-SpringBoot-Server/SpringBootCompany/src/main/java/com/mycompany/springboot/dao/MechanicDaoImpl.java
@@ -1,36 +1,36 @@
 package com.mycompany.springboot.dao;
 
-import com.mycompany.springboot.entities.Car;
-import com.mycompany.springboot.repositories.CarRepository;
+import com.mycompany.springboot.entities.Mechanic;
+import com.mycompany.springboot.repositories.MechanicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class MechanicDaoImpl implements CarDao {
+public class MechanicDaoImpl implements MechanicDao {
 
     @Autowired
-    private CarRepository carRepository;
+    private MechanicRepository MechanicRepository;
 
     @Override
-    public List<Car> get() {
-        return carRepository.findAll();
+    public List<Mechanic> get() {
+        return MechanicRepository.findAll();
     }
 
     @Override
-    public Car get(int id) {
-        return carRepository.findById(id).get();
+    public Mechanic get(int id) {
+        return MechanicRepository.findById(id).get();
     }
 
     @Override
-    public void save(Car employee) {
-        carRepository.save(employee);
+    public void save(Mechanic mechanic) {
+        MechanicRepository.save(mechanic);
     }
 
     @Override
     public void delete(int id) {
-        carRepository.deleteById(id);
+        MechanicRepository.deleteById(id);
     }
 
 }
