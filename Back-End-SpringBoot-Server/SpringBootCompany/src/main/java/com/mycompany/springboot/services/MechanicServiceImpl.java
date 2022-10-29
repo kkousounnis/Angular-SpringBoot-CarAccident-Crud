29723ed0f1ec00aeddc82ee26f37bd751bba738d
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-10-29 12:33:29 -0800

    Update MechanicServiceImpl.java

--- Back-End-SpringBoot-Server/SpringBootCompany/src/main/java/com/mycompany/springboot/services/MechanicServiceImpl.java
+++ Back-End-SpringBoot-Server/SpringBootCompany/src/main/java/com/mycompany/springboot/services/MechanicServiceImpl.java
@@ -4,10 +4,11 @@ import com.mycompany.springboot.dao.MechanicDao;
 import com.mycompany.springboot.entities.Mechanic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Repository
+@Service
 public class MechanicServiceImpl implements MechanicService {
 
     @Autowired
