bed1f99eb4bac305b49a4e81ce1e7a7da0a884e4
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-10-29 12:33:41 -0800

    Create MainServiceImpl.java

---
+++ Back-End-SpringBoot-Server/SpringBootCompany/src/main/java/com/mycompany/springboot/services/MainServiceImpl.java
@@ -0,0 +1,16 @@
+package com.mycompany.springboot.services;
+
+import com.mycompany.springboot.entities.Car;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class MainServiceImpl implements MainService{
+
+    public HashMap<String, List<Car>> createHashMap(List<Car> cars){
+
+        return (null);
+    }
+}
