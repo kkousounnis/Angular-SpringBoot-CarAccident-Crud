b2a849e0b3ef6030578f5872c61bd06c34ad14dc
Konstaninos Kousounnis <39504405+kkousounnis@users.noreply.github.com>
2022-10-29 12:33:46 -0800

    Update CarServiceImpl.java

--- Back-End-SpringBoot-Server/SpringBootCompany/src/main/java/com/mycompany/springboot/services/CarServiceImpl.java
+++ Back-End-SpringBoot-Server/SpringBootCompany/src/main/java/com/mycompany/springboot/services/CarServiceImpl.java
@@ -5,8 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import com.mycompany.springboot.dao.CarDao;
+import org.springframework.stereotype.Service;
 
-@Repository
+@Service
 public class CarServiceImpl implements CarService {
 
     @Autowired
